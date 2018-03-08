802d0d84509e55d226d04ed7572f76ff84ee3dc4
Sascha Rechenberger <sascha.rechenberger@uni-ulm.de>
2018-03-08 22:55:18 +0100

    prepared experiment

--- main.py
+++ main.py
@@ -2,33 +2,72 @@
 
 from sat.experiment import *
 from sat.prob_sat import *
+import sys
+import statistics
 
-if __name__ == '__main__':
-    root = '/media/sf_VBoxshare/SAT/'
-    dirs = { 4.0: root+'unif-k3-r4.0-v1000-c4000',
-             4.1: root+'unif-k3-r4.1-v1000-c4100',
-             4.2: root+'unif-k3-r4.2-v1000-c4200'}
+
+def ratio_cb__medianRuntime_medianEntropy(root, dirs, log, filepath, poolsize, solver):
 
     config = dict(
-        poolsize = 4,
+        poolsize = poolsize,
         verbose  = True,
-        log      = sys.stderr,
-        solver   = ProbSAT,
+        log      = log,
+        solver   = solver,
         config   = dict(
-            maxFlips = 20000,
             maxTries = 100
         )
     )
 
-    needLabel = True
+    with open(filepath, 'w') as outfile:
+        # f(vars,ratio,cb) = (medianRuntime, medianEntropy)
+        outfile.write('k,vars,ratio,cb,medianRuntime,medianEntropy\n')
+        for (k,vars, ratio), dir in dirs.items():
+            for cb in range(0, 50):
+                config['config']['cb'] = cb / 10
+                config['config']['maxFlips'] = 20 * vars
 
-    with open('k3.log', 'w') as outfile:
-        for dir in dirs.values():
-            exp = Experiment(dir, **config)
-            exp.runExperiment()
-            exp.printResults(
-                outfile = outfile,
-                label = needLabel
-            )
-            needLabel=False
+                exp = Experiment(dir, **config)
+                exp.runExperiment()
 
+                medianRuntime = []
+                medianEntropy = []
+
+                for r in exp.results:
+                    medianRuntime.append(r['totalRuntime'])
+                    medianEntropy.append(r['entropy'])
+
+                medianRuntime = statistics.median(medianRuntime)
+                medianEntropy = statistics.median(medianEntropy)
+
+                outfile.write(
+                    '{},{},{},{},{},{}\n'
+                    .format(
+                        k,
+                        r['variables'],
+                        r['ratio'],
+                        r['cb'],
+                        medianRuntime,
+                        medianEntropy
+                    )
+                )
+
+
+
+if __name__ == '__main__':
+    root = sys.argv[1]
+    dirs = { 
+        (3, 500, 4.0): os.path.join(root,'unif-k3-r4.0-v500-c2000'),
+        (3, 500, 4.1): os.path.join(root,'unif-k3-r4.1-v500-c2050'),
+        (3, 500, 4.2): os.path.join(root,'unif-k3-r4.2-v500-c2100')
+    #   (3, 1000, 4.0): os.path.join(root,'unif-k3-r4.0-v1000-c4000'),
+    #   (3, 1000, 4.1): os.path.join(root,'unif-k3-r4.1-v1000-c4100'),
+    #   (3, 1000, 4.2): os.path.join(root,'unif-k3-r4.2-v1000-c4200')
+    }
+    ratio_cb__medianRuntime_medianEntropy__3SAT500(
+        root,
+        dirs,
+        sys.stderr,
+        'experiment1.csv',
+        int(sys.argv[2]),
+        ProbSAT
+    )
