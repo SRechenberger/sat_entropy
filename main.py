324b0ddb91858a41631ccc54e9a96b1356247927
Sascha Rechenberger <sascha.rechenberger@uni-ulm.de>
2018-03-10 11:27:09 +0100

    remove data processing; just output raw data

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def experiment(
     input_directories      = dict(),
     output_directory       = '.',
     output_file_name       = 'output',
-    output_file_raw_suffix = 'raw',
+    # output_file_raw_suffix = 'raw',
     output_file_suffix     = 'csv',
     log_file_suffix        = 'log',
     logstream              = None,
@@ -39,12 +39,6 @@ def experiment(
         '{}.{}'.format(output_file_name,output_file_suffix)
     )
 
-    ## set the template for the raw data output files
-    output_file_raw_template = os.path.join(
-        output_directory,
-        '{}.{}.{}.{}'.format(output_file_name,'{}',output_file_raw_suffix,output_file_suffix)
-    )
-
     ## set up the logfile path
     log_file_path = os.path.join(
         output_directory,
@@ -58,37 +52,23 @@ def experiment(
         solver   = solver,
         prob     = prob,
         config   = dict(
-            timeLimit = 100
+            timeLimit = 10
         )
     )
 
-    raw = 0
     cbs = map(
         lambda x: x/10,
         range(0,40)
     )
     with open(output_file_path, 'w') as outfile:
         with open(log_file_path, 'w') as logfile:
-            # f(vars,ratio,cb) = (medianRuntime, medianEntropy)
-            print(
-                'k',
-                'vars',
-                'ratio',
-                'cb',
-                'medianRuntime',
-                'minimalRuntime',
-                'maximalRuntime',
-                'medianEntropy',
-                sep=',',
-                file=outfile
-            )
             totalBegin = time.time()
             log('BEGIN', [logfile, logstream])
 
+            needLabel = True
             for (k, vars, ratio), dir in dirs.items():
                 dirBegin = time.time()
                 log('  BEGIN directory {}'.format(dir), [logfile, logstream])
-
                 for cb in cbs:
                     cbBegin = time.time()
                     log('    BEGIN cb {:.2f}'.format(cb / 10), [logfile, logstream], end='')
@@ -98,41 +78,12 @@ def experiment(
 
                     exp = Experiment(dir, **config)
                     exp.runExperiment()
-                    with open(output_file_raw_template.format(raw), 'w') as fRaw:
-                        exp.printResults(
-                            outfile=fRaw,
-                            label=True,
-                        )
-                    raw += 1
-
-                    medianRuntime = []
-                    medianEntropy = []
-                    minimalRuntime = sys.maxsize
-                    maximalRuntime = -1
-
-                    for r in exp.results:
-                        medianRuntime.append(r['totalFlips'])
-                        medianEntropy.append(r['entropy'])
-                        if r['totalFlips'] > maximalRuntime:
-                            maximalRuntime = r['totalFlips']
-                        if r['totalFlips'] < minimalRuntime:
-                            minimalRuntime = r['totalFlips']
-
-                    medianRuntime = statistics.median(medianRuntime)
-                    medianEntropy = statistics.median(medianEntropy)
-
-                    print(
-                        k,
-                        r['variables'],
-                        r['ratio'],
-                        r['cb'],
-                        medianRuntime,
-                        minimalRuntime,
-                        maximalRuntime,
-                        medianEntropy,
-                        sep = ',',
-                        file = outfile
+                    exp.printResults(
+                        outfile=outfile,
+                        label=needLabel,
                     )
+                    needLabel = False
+
                     cbEnd = time.time()
                     cbTime = (cbEnd - cbBegin)
                     log(' ...END cb {} ({:10.0f} sec)'.format(cb/10, cbTime), [logfile, logstream])
@@ -151,14 +102,14 @@ def experiment(
 if __name__ == '__main__':
     root = sys.argv[1]
     dirs = {
-    #   (3, 10, 4.0): os.path.join(root,'unif-k3-r4.0-v10-c40'),
+       (3, 10, 4.0): os.path.join(root,'unif-k3-r4.0-v10-c40'),
     #   (3, 10, 4.1): os.path.join(root,'unif-k3-r4.1-v10-c41'),
     #   (3, 10, 4.2): os.path.join(root,'unif-k3-r4.2-v10-c42')
     #   (3, 500, 4.0): os.path.join(root,'unif-k3-r4.0-v500-c2000'),
     #   (3, 500, 4.1): os.path.join(root,'unif-k3-r4.1-v500-c2050'),
     #   (3, 500, 4.2): os.path.join(root,'unif-k3-r4.2-v500-c2100')
     #   (3, 1000, 4.0): os.path.join(root,'unif-k3-r4.0-v1000-c4000'),
-       (3, 1000, 4.1): os.path.join(root,'unif-k3-r4.1-v1000-c4100'),
+    #   (3, 1000, 4.1): os.path.join(root,'unif-k3-r4.1-v1000-c4100'),
     #   (3, 1000, 4.2): os.path.join(root,'unif-k3-r4.2-v1000-c4200'),
     #   (3, 1000, 4.26): os.path.join(root,'unif-k3-r4.26-v1000-c4260')
     }
@@ -169,6 +120,6 @@ if __name__ == '__main__':
         output_file_name  = sys.argv[3],
         poolsize          = int(sys.argv[4]),
         logstream         = sys.stderr,
-        prob              = 0.2,
+        prob              = 1,
         verbose           = False
     )
