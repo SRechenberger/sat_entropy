07e22af110ecb25998bed0e93696398f51704525
Sascha Rechenberger <sascha.rechenberger@uni-ulm.de>
2018-03-13 22:19:29 +0100

    experiments in separate script

---
+++ experiments.py
@@ -0,0 +1,58 @@
+from sat.prob_sat import ProbSAT
+
+def float_range_list(begin, end, step):
+    factor = 1
+    while (step * factor) % 1 > 0:
+        factor *= 10
+
+    return list(
+        map(
+            lambda x: x/factor,
+            range(
+                int(begin*factor),
+                int(end*factor),
+                int(step*factor)
+            )
+        )
+    )
+
+
+
+experiments = {
+    'k3-v20': dict(
+        dirs   = [
+            'unif-k3-r4.0-v20-c80',
+            'unif-k3-r4.1-v20-c82'
+        ],
+        solver = ProbSAT,
+        prob   = 10,
+        config = dict(
+            cb = float_range_list(2,3,0.1),
+            maxFlips = 50 * 20,
+            timeLimit = 10
+        )
+    ),
+    'k3-v500-r4.1-default-cb': dict(
+        dirs   = ['unif-k3-r4.1-v500-c2050'],
+        solver = ProbSAT,
+        prob   = 100,
+        config = dict(
+            maxFlips = 50 * 500,
+            timeLimit = 20
+        )
+    ),
+    'k3-v500-r4.1-default-cb-early-restart': dict(
+        dirs   = ['unif-k3-r4.1-v500-c2050'],
+        solver = ProbSAT,
+        prob   = 500,
+        config = dict(
+            maxFlips = 50 * 500,
+            lookBack = 2 * 500,
+            minEntropyF = float_range_list(0.5,0.9,0.1),
+            timeLimit = 20,
+        )
+    )
+}
+
+short_cut = {i:k for k,i in zip(experiments.keys(), range(0,len(experiments)))}
+
