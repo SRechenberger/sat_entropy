0af2d2dd107a55890e1b3cde4daabdada28e1267
Sascha Rechenberger <sascha.rechenberger@uni-ulm.de>
2018-05-15 08:47:25 +0200

    fixed job file

--- jobs/k3-v500-r4.2-er.sh
+++ jobs/k3-v500-r4.2-er.sh
@@ -2,8 +2,8 @@
 #MSUB -l nodes=1:ppn=16
 #MSUB -l walltime=3:00:00
 #MSUB -l pmem=2000mb
-#MSUB -N k3-v500-r4.2
-#MSUB -o k3-v500-r4.2.out
+#MSUB -N k3-v500-r4.2-er
+#MSUB -o k3-v500-r4.2-er.out
 #MSUB -M sascha.rechenberger@uni-ulm.de
 #MSUB -m bea
 #MSUB -q singlenode
