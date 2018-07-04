5ef3319b621d731a1e23edec79a528a6c35c7972
Sascha Rechenberger <sascha.rechenberger@uni-ulm.de>
2018-07-04 11:21:01 +0200

    extended max_entropy_variable

--- sat/utils.py
+++ sat/utils.py
@@ -101,10 +101,11 @@ class Entropytracker:
         return entropy
 
     def max_entropy_variable(self):
-        return max(
+        l = max(
             list(enumerate(self.count))[1:],
             key = lambda x: self.h[self.count[x]]
         )
+        return l, self.h[self.count[l]], self.count[l]/self.size
 
     def add(self, elem):
         """ Adds a new element to the queue, and updates the entropy,
